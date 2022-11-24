9f188e8747f87a3272e97bc8606e006246fc885e
강인영 <kiy@gang-in-yeong-ui-MacBookAir.local>
2022-11-24 15:25:53 +0900

    feat: User can check parking stauts

---
+++ client/src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component {
+	render () {
+		return (
+			<div>
+				<h1 className="title-container__title">USPACE</h1>
+				<h3 className="title-container__subtitle">Make your own space, USPACE</h3>
+			</div>
+		);
+	}
+}
+
+export default Titles;
